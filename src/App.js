a33014ffb071603f047c2200236aabf5440315ac
dalpang81 <leeoy2000@naver.com>
2023-02-17 13:09:48 +0900

    #7. 마무리

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import { useEffect, useState } from "react";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [myMoney, setMyMoney] = useState([]);
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers").then((response) => response.json()).then((json) => {setCoins(json); setLoading(false);});
-  }, []);
-
-  function onChange(event) {
-    setMyMoney(event.target.value);
-  }
-
-    return(
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      <input onChange={onChange} value = {myMoney} type="nuber" placeholder="Please write your USD" />
-      {loading ? <strong>Loading...</strong> :       <select>
-        {coins.map((coin) => (<option key={coin.id}>{coin.name} ({coin.symbol}): ${myMoney / coin.quotes.USD.price} USD</option>))}
-      </select>}
-      
-    </div>);
+  return (
+    <Router>
+      <Switch>
+        <Route path="/hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
